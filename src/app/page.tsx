8fea522f88916bb56b1b6987714a894d0b95a302
manavmalaviya <manavmalaviya27@gmail.com>
2025-07-13 05:04:15 +1000

    landign page gif layout changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="flex justify-center mx-1">
-      <img src="/nira-1.gif" className="flex flex-1 object-cover" alt="Nira Diamond Jewelry" />
+    <div className="flex justify-center mx-1 h-screen">
+      <Image fill src="/nira-1.gif" className="object-cover" alt="Nira Diamond Jewelry" />
     </div>
   );
 }
