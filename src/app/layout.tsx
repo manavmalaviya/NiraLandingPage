9f98a454c4faea676a9577a8dd99dbe47004c4b1
manavmalaviya <manavmalaviya27@gmail.com>
2025-07-13 04:33:29 +1000

    nira landing page added web app icon and removed uncessary ones ready to deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import HeaderProvider from "@/context/HeaderProvider";
 import MobileHeader from "@/components/MobileHeader";
+import Head from "next/head";
 
 const primary_font = Cormorant_Garamond({
   subsets: ['latin'],
@@ -32,6 +33,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${primary_font.variable} ${secondary_font.variable}  font-sans  antialiased`}>
+       <Head>
+        <link rel="icon" href="/favicon.ico" />
+        {/* <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        /> */}
+        <link
+          rel="apple-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body>
         <HeaderProvider>
           <div id="scrollable-div" className="flex flex-col scrollable h-screen overflow-y-auto relative">
