d1e4138523dcbebaaa22a572becb6090b89cbd4e
manavmalaviya <manavmalaviya27@gmail.com>
2025-07-13 12:08:55 +1000

    image aspect changes for web and mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+'use client';
+import { useGetWindowSize } from "@/hooks/useWindowsize";
 import Image from "next/image";
 
 export default function Home() {
+  const windowSize = useGetWindowSize();
   return (
-    <div className="flex justify-center mx-1 h-screen">
-      <Image unoptimized fill src="/nira-1.gif" className="object-cover" alt="Nira Diamond Jewelry" />
+    <div className={`mx-1 ${windowSize=='sm'? "relative w-full aspect-video":"flex justify-center h-screen"}`}>
+      <Image unoptimized fill src="/nira-1.gif" className={"object-cover"} alt="Nira Diamond Jewelry" />
     </div>
   );
 }
