c5e11f1eb83145253e6bd38cb152311cad2a6d5e
manavmalaviya <manavmalaviya27@gmail.com>
2025-07-13 11:39:55 +1000

    image changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex justify-center mx-1 h-screen">
-      <Image fill src="/nira-1.gif" className="object-cover" alt="Nira Diamond Jewelry" />
+      <Image unoptimized fill src="/nira-1.gif" className="object-cover" alt="Nira Diamond Jewelry" />
     </div>
   );
 }
